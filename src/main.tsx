ad692617515ef025680a70d78d2473f459295ecf
Eduard <135214414+eduard-if@users.noreply.github.com>
2024-01-16 09:35:27 +0200

    Add Mantine providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import '@mantine/core/styles.css';
+import {MantineProvider} from '@mantine/core';
+import '@mantine/notifications/styles.css';
+import {Notifications} from '@mantine/notifications';
+import '@mantine/dates/styles.css';
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <MantineProvider defaultColorScheme='dark'>
+      <Notifications position='bottom-right' />
+      <App />
+    </MantineProvider>
+  </React.StrictMode>
+);
